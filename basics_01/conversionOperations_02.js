542d68be89ac8b32669a1d222a10f0e792f39fa0
amritahazra692 <amritahazra7@gmail.com>
2026-09-10 11:35:44 +0000

    basics of js

--- basics_01/conversionOperations_02.js
+++ basics_01/conversionOperations_02.js
@@ -25,5 +25,21 @@ let newNumber = 21
 
 let isnewNumber = String(newNumber)
 
-console.log(isnewNumber);
-console.log(typeof isnewNumber);
+// console.log(isnewNumber);
+// console.log(typeof isnewNumber);
+
+/*************************Operations****************/
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
