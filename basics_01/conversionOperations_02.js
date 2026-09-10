cf3285985f50ba5de594ba5ac48a22db0cfa67c5
amritahazra692 <amritahazra7@gmail.com>
2026-09-10 07:55:01 +0000

    Conversion of datatypes

---
+++ basics_01/conversionOperations_02.js
@@ -0,0 +1,29 @@
+let score = "33abc"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" --> 33 (conversion from string to number)
+// "33abc" --> NaN (Not a number)
+// true --> 1 (boolean)
+
+let isLoggedIn = 0
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+
+//console.log(booleanisLoggedIn);
+
+// 1 --> true;  0--> false
+// "" --> false;  "string" --> true
+
+let newNumber = 21
+
+let isnewNumber = String(newNumber)
+
+console.log(isnewNumber);
+console.log(typeof isnewNumber);
